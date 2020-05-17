c53c171e8621cb7e6e2ea0cd44cfdead7ec10010
Derek phung <dragevards@gmail.com>
2020-05-17 00:33:40 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -62,7 +62,9 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.chomp.to_i
     if (1..Song.all.length).include?(input)
-      song = Song.all.sort_by
+      song = Song.all.sort_by(&:name)(input - 1)
+    end
+    puts ""
     
     
 end
