49a95e43eb6faa223d35a5b045034226dd15b223
Derek phung <dragevards@gmail.com>
2020-05-17 00:27:27 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -35,5 +35,7 @@ class MusicLibraryController
   def list_songs_by_artist 
     puts "Please enter the name of an artist:"
     input = gets.chomp
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort_by()
   
 end
