08e519af24b990a9e2153f3a544193b44710da23
Derek phung <dragevards@gmail.com>
2020-05-17 00:21:37 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -5,24 +5,11 @@ class MusicLibraryController
     file.import
   end
   
-  def call 
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'.
-    
-  
   def list_songs
     sorted_songs = Song.all.sort_by do |song| 
-      song.name 
-    end 
-    sorted_songs.each.with_index(1) do |index, song|
-      puts 
-    end
-  
-  
+        song.name 
+      end 
+      sorted_songs.each.with_index(1) do |index, song|
+        puts "#{index}. #(song.artist.name} - )
+      end
 end
