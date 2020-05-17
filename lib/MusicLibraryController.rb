eeb3326106cfc13dc93faf465e92e5ddf69553e5
Derek phung <dragevards@gmail.com>
2020-05-17 00:51:03 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -15,8 +15,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    
     input = gets.chomp
     case input 
+    
     when "list songs"
       list_songs 
     when "list artists"
