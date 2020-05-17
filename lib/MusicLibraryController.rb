54c86084aabb537fcd3a946b2444f66392143933
Derek phung <dragevards@gmail.com>
2020-05-17 00:44:16 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -36,58 +36,45 @@ class MusicLibraryController
     end
   end
   
-  def list_songs
-    sorted_songs = Song.all.sort_by do |song| 
-      song.name 
-    end 
-    sorted_songs.each.with_index(1) do |index, song|
+   def list_songs 
+    Song.all.sort_by(&:name).each.with_index(1) do |song,index| 
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    end
-  end
-  
-  def list_artists
-    sorted_artists = Artist.all.sort_by do |artist|
-      artist.name 
     end 
-    sorted_artists.each.with_index(1) do |index, artist| 
+  end 
+  
+  def list_artists 
+    Artist.all.sort_by(&:name).each.with_index(1) do |artist, index|
       puts "#{index}. #{artist.name}"
     end
   end
   
   def list_genres
-    sorted_genres = Genre.all.sort_by do |genre|
-      genre.name 
-    end
-    sorted_genres.each.with_index(1) do |index, genre|
+    Genre.all.sort_by(&:name).each.with_index(1) do |genre, index|
       puts "#{index}. #{genre.name}"
     end
   end
   
   def list_songs_by_artist 
     puts "Please enter the name of an artist:"
-    input = gets.chomp
-    if artist = Artist.find_by_name(input)
-      sorted_songs = artist.songs.sort_by do |song|
-        song.name 
-      end
-      sorted_songs.each.with_index(1) do |index, song| 
+    input = gets.strip
+    
+    if artist = Artist.find_by_name(input) 
+      artist.songs.sort_by(&:name).each.with_index(1) do |song, index|
         puts "#{index}. #{song.name} - #{song.genre.name}"
-      end
-    end
-  end
+      end 
+    end 
+  end 
   
-  def list_songs_by_genre
+  def list_songs_by_genre 
     puts "Please enter the name of a genre:"
-    input = gets.chomp
-    if genre = Genre.find_by_name(input)
-      sorted_songs= genre.songs.sort_by do |song|
-        song.name
-      end
-      songs_sorted_by_name.each.with_index(1) do |song,index|
+    input = gets.strip
+    
+    if genre = Genre.find_by_name(input) 
+      genre.songs.sort_by(&:name).each.with_index(1) do |song, index|
         puts "#{index}. #{song.artist.name} - #{song.name}"
-      end
-    end
-  end
+      end 
+    end 
+  end 
   
   def play_song 
     puts "Which song number would you like to play?"
